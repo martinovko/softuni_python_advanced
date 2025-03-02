910dc48c7a2575a0b8e3d1ae28112e1a77203803
martinovko <martinovko@gmail.com>
2025-03-02 22:17:20 +0200

    doing exercises :-)

---
+++ 9_exercise_multidimensional_lists/ex_10.py
@@ -0,0 +1,82 @@
+def bunny_spreader(matrix):
+    bunnies_cor = []
+    lost = False
+    for row in range(len(matrix)):
+        for col in range(len(matrix[row])):
+            if matrix[row][col] == 'B':
+                bunnies_cor.append([row, col])
+    for b in range(len(bunnies_cor)):
+        r, c = bunnies_cor[b]
+        if valid_index(r, (c - 1), matrix):
+            if matrix[r][c - 1] == 'P':
+                lost = True
+            matrix[r][c - 1] = 'B'
+        if valid_index(r, (c + 1), matrix):
+            if matrix[r][c + 1] == 'P':
+                lost = True
+            matrix[r][c + 1] ='B'
+        if valid_index(r - 1, c, matrix):
+            if matrix[r - 1][c] == 'P':
+                lost = True
+            matrix[r - 1][c] = 'B'
+        if valid_index((r + 1), c, matrix):
+            if matrix[r + 1][c] == 'P':
+                lost = True
+            matrix[r + 1][c] = 'B'
+    return lost, matrix
+
+def valid_index(index1, index2, matrix):
+    return 0 <= index1 < len(matrix) and 0 <= index2 < len(matrix[0])
+
+n, m = [int(x) for x in input().split()]
+lair = [[x for x in input()] for _ in range(n)]
+commands = [x for x in input()]
+player_r, player_c = 0, 0
+win = False
+game_lost = False
+
+for r in range(len(lair)):
+    for c in range(len(lair[r])):
+        if lair[r][c] == 'P':
+            player_r, player_c = r, c
+
+
+for i in range(len(commands)):
+    lair[player_r][player_c] = '.'
+    if commands[i] == 'L':
+        if valid_index(player_r,(player_c - 1), lair):
+            player_c -= 1
+            lair[player_r][player_c] = 'P'
+        else:
+            win = True
+        game_lost, lair = bunny_spreader(lair)
+    elif commands[i] == 'R':
+        if valid_index(player_r, (player_c + 1), lair):
+            player_c += 1
+            lair[player_r][player_c] = 'P'
+        else:
+            win = True
+        game_lost, lair = bunny_spreader(lair)
+    elif commands[i] == 'U':
+        if valid_index((player_r - 1), player_c, lair):
+            player_r -= 1
+            lair[player_r][player_c] = 'P'
+        else:
+            win = True
+        game_lost, lair = bunny_spreader(lair)
+    elif commands[i] == 'D':
+        if valid_index((player_r + 1), player_c, lair):
+            player_r += 1
+            lair[player_r][player_c] = 'P'
+        else:
+            win = True
+        game_lost, lair = bunny_spreader(lair)
+    if win or game_lost:
+        break
+
+[print(*rows, sep='') for rows in lair]
+if win:
+    print(f'won: {player_r} {player_c}')
+else:
+    print(f'dead: {player_r} {player_c}')
+
---
+++ 9_exercise_multidimensional_lists/ex_9.py
@@ -0,0 +1,81 @@
+def player (matrix): #finds 's' in the matrix to know where to start
+    for r in range(len(matrix)):
+        for c in range(len(matrix[r])):
+            if matrix[r][c] == 's':
+                return r, c #return coordinates
+    return None
+
+def coal_counter(matrix):
+    coals = 0
+    for r in matrix:
+        for c in r:
+            if c == 'c':
+                coals += 1
+    return coals
+
+def valid_index(index1, index2, matrix):
+    return 0 <= index1 < len(matrix) and 0 <= index2 < len(matrix[0])
+
+
+n = int(input())
+commands = input().split()
+field = [[x for x in input().split()] for _ in range(n)]
+
+miner_r, miner_c = player(field)
+coal = coal_counter(field)
+game_on = True
+new_r, new_c = 0, 0
+
+while commands:
+    if commands[0] == 'left':
+        commands.pop(0)
+        if valid_index(miner_r, (miner_c - 1), field):
+            new_r, new_c = miner_r, miner_c - 1
+            if field[new_r][new_c] == "c":
+                coal -= 1
+                field[new_r][new_c] = '*'
+            if field[new_r][new_c] =='e':
+                game_on = False
+        miner_r, miner_c = new_r, new_c
+
+
+    elif commands[0] == 'right':
+        commands.pop(0)
+        if valid_index(miner_r, (miner_c + 1), field):
+            new_r, new_c = miner_r, miner_c + 1
+            if field[new_r][new_c] == "c":
+                coal -= 1
+                field[new_r][new_c] = '*'
+            if field[new_r][new_c] =='e':
+                game_on = False
+        miner_r, miner_c = new_r, new_c
+
+    elif commands[0] == 'up':
+        commands.pop(0)
+        if valid_index((miner_r - 1), miner_c, field):
+            new_r, new_c = miner_r - 1, miner_c
+            if field[new_r][new_c] == "c":
+                coal -= 1
+                field[new_r][new_c] = '*'
+            if field[new_r][new_c] =='e':
+                game_on = False
+        miner_r, miner_c = new_r, new_c
+
+    elif commands[0] == 'down':
+        commands.pop(0)
+        if valid_index((miner_r + 1), miner_c, field):
+            new_r, new_c = miner_r + 1, miner_c
+            if field[new_r][new_c] == "c":
+                coal -= 1
+                field[new_r][new_c] = '*'
+            if field[new_r][new_c] =='e':
+                game_on = False
+        miner_r, miner_c = new_r, new_c
+
+    if coal < 1:
+        print(f'You collected all coal! ({miner_r}, {miner_c})')
+        break
+    if not game_on:
+        print(f'Game over! ({miner_r}, {miner_c})')
+if coal > 1 and game_on:
+    print(f'{coal} pieces of coal left. ({miner_r}, {miner_c})')
